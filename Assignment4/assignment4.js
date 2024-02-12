3154fc2463eddf0cb60e859548b5b2c57354d643
rasmussvala <rassv453@student.liu.se>
2024-02-12 17:03:11 +0100

    read file

--- Assignment4/assignment4.js
+++ Assignment4/assignment4.js
@@ -1,7 +1,40 @@
-const data = d3
-  .json("./starwars-interactions/starwars-full-interactions-allCharacters.json")
+let data = await d3
+  .json(
+    "./starwars-interactions/starwars-episode-1-interactions-allCharacters.json"
+  )
   .catch(function (error) {
     console.error("Error loading data:", error);
   });
 
+const createDiagram = (svgId, data) => {
+  const svg = d3.select(`#${svgId}`);
+
+  const minDomain = d3.min(data.nodes, function (d) {
+    return d.value;
+  });
+
+  const maxDomain = d3.max(data.nodes, function (d) {
+    return d.value;
+  });
+
+  const minRadius = 10;
+  const maxRadius = 20;
+
+  const sizeScale = d3
+    .scaleLinear()
+    .domain([minDomain, maxDomain])
+    .range([minRadius, maxRadius]);
+
+  const nodes = svg
+    .append("g")
+    .selectAll("circle")
+    .data(data.nodes)
+    .enter()
+    .append("circle")
+    .attr("r", (d) => sizeScale(30))
+    .attr("fill", (d) => d.colour);
+};
+
 console.log(data);
+
+createDiagram("diagram1", data);
