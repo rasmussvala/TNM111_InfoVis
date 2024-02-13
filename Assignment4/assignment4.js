b4b03f7809838e779beeb601a5e58cdb0052de3a
Emil Wallberg <ew2001@live.se>
2024-02-13 14:50:26 +0100

    Working graph with wrong data

--- Assignment4/assignment4.js
+++ Assignment4/assignment4.js
@@ -1,3 +1,6 @@
+var width = 400,
+  height = 300;
+
 let data = await d3
   .json(
     "./starwars-interactions/starwars-episode-1-interactions-allCharacters.json"
@@ -5,7 +8,27 @@ let data = await d3
   .catch(function (error) {
     console.error("Error loading data:", error);
   });
+var nodes = [
+  { name: "A" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+];
 
+var links = [
+  { source: 0, target: 1 },
+  { source: 0, target: 2 },
+  { source: 0, target: 3 },
+  { source: 1, target: 6 },
+  { source: 3, target: 4 },
+  { source: 3, target: 7 },
+  { source: 4, target: 5 },
+  { source: 4, target: 7 },
+];
 const createDiagram = (svgId, data) => {
   const svg = d3.select(`#${svgId}`);
 
@@ -25,14 +48,58 @@ const createDiagram = (svgId, data) => {
     .domain([minDomain, maxDomain])
     .range([minRadius, maxRadius]);
 
-  const nodes = svg
-    .append("g")
-    .selectAll("circle")
-    .data(data.nodes)
-    .enter()
-    .append("circle")
-    .attr("r", (d) => sizeScale(30))
-    .attr("fill", (d) => d.colour);
+  // Create and configure the simulation
+  var simulation = d3
+    .forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("link", d3.forceLink().links(links))
+    .on("tick", ticked);
+
+  function updateLinks() {
+    d3.select(".links")
+      .selectAll("line")
+      .data(links)
+      .join("line")
+      .attr("x1", function (d) {
+        return d.source.x;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("x2", function (d) {
+        return d.target.x;
+      })
+      .attr("y2", function (d) {
+        return d.target.y;
+      });
+  }
+
+  function updateNodes() {
+    d3.select(".nodes")
+      .selectAll("text")
+      .data(nodes)
+      .join("text")
+      .text(function (d) {
+        return d.name;
+      })
+      //.attr("r", (d) => sizeScale(30))
+      //.attr("fill", (d) => d.colour)
+      .attr("x", function (d) {
+        return d.x;
+      })
+      .attr("y", function (d) {
+        return d.y;
+      })
+      .attr("dy", function (d) {
+        return 5;
+      });
+  }
+
+  function ticked() {
+    updateLinks();
+    updateNodes();
+  }
 };
 
 console.log(data);
