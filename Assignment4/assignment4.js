535f6a667fcf055ec6c41c90992a35048fa7f304
rasmussvala <rassv453@student.liu.se>
2024-02-21 11:13:44 +0100

    added slider with no functionality

--- Assignment4/assignment4.js
+++ Assignment4/assignment4.js
@@ -5,8 +5,6 @@ async function main() {
 
   createDiagram("diagram1", data1);
   createDiagram("diagram2", data2);
-
-  handleRangeInputs();
 }
 
 main();
@@ -185,40 +183,6 @@ function createDiagram(svgId, data) {
   }
 }
 
-function handleRangeInputs() {
-  var lowerSlider = document.querySelector("#lower");
-  var upperSlider = document.querySelector("#upper");
-
-  var lowerVal = parseInt(lowerSlider.value);
-  var upperVal = parseInt(upperSlider.value);
-
-  upperSlider.oninput = function () {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
-
-    if (upperVal < lowerVal + 4) {
-      lowerSlider.value = upperVal - 4;
-
-      if (lowerVal == lowerSlider.min) {
-        upperSlider.value = 4;
-      }
-    }
-  };
-
-  lowerSlider.oninput = function () {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
-
-    if (lowerVal > upperVal - 4) {
-      upperSlider.value = lowerVal + 4;
-
-      if (upperVal == upperSlider.max) {
-        lowerSlider.value = parseInt(upperSlider.max) - 4;
-      }
-    }
-  };
-}
-
 async function loadData() {
   try {
     const fileNames = [
@@ -282,3 +246,96 @@ function linkTooltip(node1, node2, value, svgId) {
     }
   }
 }
+
+/* ------------------ Multi-Slider functionality  ------------------ */
+
+const fillColor = "#0075ff";
+
+function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", fillColor, controlSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromSlider.value = from;
+  }
+}
+
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", fillColor, controlSlider);
+  setToggleAccessible(toInput);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+  }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", fillColor, toSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromInput.value = from;
+  }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", fillColor, toSlider);
+  setToggleAccessible(toSlider);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+    toSlider.value = from;
+  }
+}
+
+function getParsed(currentFrom, currentTo) {
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+  const rangeDistance = to.max - to.min;
+  const fromPosition = from.value - to.min;
+  const toPosition = to.value - to.min;
+  controlSlider.style.background = `linear-gradient(
+    to right,
+    ${sliderColor} 0%,
+    ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+    ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
+    ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+  const toSlider = document.querySelector("#toSlider");
+  if (Number(currentTarget.value) <= 0) {
+    toSlider.style.zIndex = 2;
+  } else {
+    toSlider.style.zIndex = 0;
+  }
+}
+
+const fromSlider = document.querySelector("#fromSlider");
+const toSlider = document.querySelector("#toSlider");
+const fromInput = document.querySelector("#fromInput");
+const toInput = document.querySelector("#toInput");
+fillSlider(fromSlider, toSlider, "#C6C6C6", fillColor, toSlider);
+setToggleAccessible(toSlider);
+
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromInput.oninput = () =>
+  controlFromInput(fromSlider, fromInput, toInput, toSlider);
+toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
